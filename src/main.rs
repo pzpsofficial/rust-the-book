b986f87b28e27f6adad15deb3e878f1ee2b81100
Bartosz Karpiel <pzpsofficial@gmail.com>
2024-01-11 19:47:24 +0100

    feat(chapter-3): data types & functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    let y = five();
+    let z = plus_one(100);
+    another_function(z, 'b');
+}
+
+fn another_function(x: i32, unit_label: char) {
+    println!("The value of x is: {x}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
